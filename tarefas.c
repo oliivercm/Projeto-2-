3f8e6cab452489359cfa2654fefacfac539f7ffd
Cecília Martins De Oliveira <135181058+oliivercm@users.noreply.github.com>
2024-04-12 22:45:47 -0300

    Update tarefas.c

--- tarefas.c
+++ tarefas.c
@@ -2,6 +2,49 @@
 #include <string.h>
 #include "tarefas.h"
 
+ERROS criarTarefa(Tarefa tarefas[], int *pos){
+    if(*pos >= TOTAL_TAREFAS)
+        return MAX_TAREFA;
+
+    printf("Entre com a prioridade: ");
+    scanf("%d", &tarefas[*pos].prioridade);
+    clearBuffer();
+    printf("Entre com a categoria: ");
+    fgets(tarefas[*pos].categoria, 100, stdin);
+
+    printf("Entre com a descricao: ");
+    fgets(tarefas[*pos].descricao, 300, stdin);
+
+    *pos = *pos + 1;
+
+    return OK;
+}
+
+ERROS deletarTarefa(Tarefa tarefas[], int *pos){
+    // teste se existem tarefas
+    if(*pos == 0)
+        return SEM_TAREFAS;
+
+    // verifica se a tarefa escolhida existe
+    int pos_deletar;
+    printf("Entre com a posicao da tarefa a ser deletada: ");
+    scanf("%d", &pos_deletar);
+    pos_deletar--; // garantir posicao certa no array
+    if(pos_deletar >= *pos || pos_deletar < 0)
+        return NAO_ENCONTRADO;
+
+    for(int i = pos_deletar; i < *pos; i++){
+        tarefas[i].prioridade = tarefas[i+1].prioridade;
+        strcpy(tarefas[i].categoria, tarefas[i+1].categoria);
+        strcpy(tarefas[i].descricao,  tarefas[i+1].descricao);
+    }
+
+    *pos = *pos - 1;
+
+    return OK;
+}
+
+
 ERROS listarTarefas(Tarefa tarefas[], int *pos) {
     if (*pos == 0) {
         printf("Sem tarefas.\n");
@@ -72,4 +115,7 @@ ERROS carregarTarefas(Tarefa tarefas[], int *pos) {
     return OK;
 }
 
-//teste
+void clearBuffer(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
